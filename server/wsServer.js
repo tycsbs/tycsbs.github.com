dd571a09d94403258110195c2f2e483a515994ec
tycsbs <tutebaisong@163.com>
2017-10-26 23:37:27 +0800

    add uer login page

--- server/wsServer.js
+++ server/wsServer.js
@@ -2,17 +2,22 @@ var app = require('http').createServer()
 var io = require('socket.io')(app)
 var PORT = 3000
 app.listen(PORT)
-io.on('connection',function(socket){
-	socket.on('enter',(str) => {
-		io.emit('enter',`${str.name}: 上线`)
-	})
+var roomUser = []
+var mine
+io.on('connection', function (socket) {
+    socket.on('login', function (user) {
+        roomUser.push(user.name)
+        mine = user.name
+        io.emit('login', user)
+    })
 
-	socket.on('msg',(str) => {
-		io.emit("msg",`${str.name}: ${str.text}`)
-	})
-	socket.on('disconnect',(str) => {
-		io.emit('leave',`${str.name}: 离线`)
-	})
+    socket.on('msg', (str) => {
+        // io.emit("msg", `${str.name}: ${str.text}`)
+        io.emit("msg", str)
+    })
+    socket.on('disconnect', () => {
+        io.emit('leave', {name:mine})
+    })
 })
 
 console.log(`websocket server listening on port ${PORT}`)
